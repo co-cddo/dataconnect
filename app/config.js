3c268df37ba8e018e967cb992a984343a20068de
Charles Baird <charles.baird@digital.cabinet-office.gov.uk>
2022-03-15 23:57:44 +0000

    change name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'DataConnect21',
+  serviceName: 'DataConnect',
 
   // Default port that prototype runs on
   port: '3000',
